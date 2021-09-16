8ac6f81e91adf381867e980db24bcccda4f8bc9a
Fernando Petry <fernando.petry@autoavaliar.com.br>
2021-09-16 01:14:31 -0300

    commit inicial

--- src/Circle.test.ts
+++ src/Circle.test.ts
@@ -1,6 +1,8 @@
 import Circle from "./Circle";
 
-test("Should calculate the area of circle", function () {
-    const circle = new Circle(2);
-    expect(circle.getArea()).toBe(12.566370614359172);
+describe("Tests", () => {
+    test("Should calculate the area of circle", function () {
+        const circle = new Circle(2);
+        expect(circle.getArea()).toBe(12.566370614359172);
+    });
 });
